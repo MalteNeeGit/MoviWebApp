bc3e58d3943bba74bb6ab46373f3295b0dc1c1fe
Malte Neemann <malteneemann129@gmail.com>
2026-04-02 15:10:05 +0200

    Setup data models for users and movies and the data_manager to handle the tables

---
+++ data_manager.py
@@ -0,0 +1,41 @@
+from models import db, User, Movie
+
+class DataManager():
+    # Define Crud operations as methods
+    def create_user(self,name):
+        """Creates a new user"""
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def get_users(self):
+        """ Returns a list of all users"""
+        users = db.session.query(User).all()
+
+        return users
+
+    def get_movies(self, user_id):
+        """Gets the movies from a particular user and returns the Movies as a list"""
+        user = db.session.query(User)\
+        .filter(User.id == user_id).one()
+
+        return user.movies
+
+    def add_movie(self,movie):
+        """Adds a movie to the users database"""
+        db.session.add(movie)
+        db.session.commit()
+
+    def update_movie(self, movie_id, new_title):
+        """Updates the title of a Movie"""
+        movie_to_update = db.session.query(Movie)\
+        .filter(Movie.id == movie_id).one()
+
+        movie_to_update.name = new_title
+        db.session.commit()
+
+    def delete_movie(self, movie_id):
+        """Deletes a movie from a database"""
+        db.session.query(Movie)\
+        .filter(Movie.id == movie_id).delete()
+        db.session.commit()
---
+++ models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+#Datenbankobjekt
+db = SQLAlchemy()
+
+#zwischentabelle für many-to-many-beziehungen
+user_movies = db.Table('user_movies',
+                       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                       db.Column('movie_id', db.Integer,db.ForeignKey('movie.id'))
+                       )
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    movies = db.relationship('Movie', secondary=user_movies, backref='users')
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    director = db.Column(db.String(200))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(500))
+
+
+
